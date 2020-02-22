c6e712917c33cfbe73cc33239cbac7f0d03057f6
Lynette Abney <shglynette321@yahoo.com>
2020-02-22 14:12:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,17 @@
-
+// irb
 def display_board
   puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
+def display_board
+  puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  end
